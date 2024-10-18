d8ef0cadbb7f8e9f43485aed33d3901e7a252f60
HIROKIASANO99 <asanohiroki0722@yahoo.co.jp>
2024-10-18 19:55:09 +0900

    mmm

--- debug.c
+++ debug.c
@@ -1,16 +1,19 @@
 
 
-#include"./include/push_swap.h"
+#include "./include/push_swap.h"
 
-void print_list(t_list *lst, char c)									
-{													
-    int index = 1;											
-    while (lst != NULL)											
-    {													
-        printf("stack_%c[%d]	: number = %d	: order = %d	: group = %d	:address = %p\n",			
-               c, index, lst->number, lst->order, lst->group, lst->next);						
-        lst = lst->next;										
-        index++;											
-    }													
-printf("\n");
-}													
+void	print_list(t_list *lst, char c)
+{
+	int	index;
+
+	index = 1;
+	while (lst != NULL)
+	{
+		printf("stack_%c[%d]	: number = %d	: order = %d	: group =
+							%d	:address = %p\n", c, index, lst->number,
+							lst->order, lst->group, lst->next);
+		lst = lst->next;
+		index++;
+	}
+	printf("\n");
+}
