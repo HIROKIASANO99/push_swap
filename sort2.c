25455c105b1c587356c8e8b195e70dc187265967
HIROKIASANO99 <asanohiroki0722@yahoo.co.jp>
2024-10-22 11:30:29 +0900

    mkmkmk

--- sort2.c
+++ sort2.c
@@ -10,21 +10,20 @@ int ft_order_pattern(t_list **stack_a, int count_str)
 	int pattern;
 	int digit;
 	int j;
-
 	
 	pattern = 0;
 	stack = *stack_a;
 	while(stack != NULL)
 	{
 		digit = 1;
-		j = 0;
+		j = 1;
 		while(j != count_str)
 		{
 			digit = digit * 10;
 			j++;
 		}
-		pattern = stack->order * digit;
-		count_str;
+		pattern = pattern + stack->order * digit;
+		count_str--;
 		stack = stack->next;
 	}
 	return(pattern);
@@ -38,9 +37,17 @@ void ft_min_sort(t_list **stack_a ,int count_str)
 	if(count_str == 3)
 	{
 		pattern = ft_order_pattern(stack_a, count_str);
-	}
-	if(count_str == 5)
-	{
-		pattern = ft_order_pattern(stack_a, count_str);
+		if(pattern == 123)		
+			ft_printf("1\n");		
+		if(pattern == 132)		
+			ft_printf("2\n");		
+		if(pattern == 213)		
+			ft_printf("3\n");		
+		if(pattern == 231)		
+			ft_printf("4\n");		
+		if(pattern == 321)		
+			ft_printf("5\n");		
+		if(pattern == 312)		
+			ft_printf("6\n");		
 	}
 }
